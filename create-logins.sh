7da5680fbe38a14faf8c84cb98d9cd5d19396a09
root <root@ns70.biz-land.com>
2023-07-19 10:16:09 -0500

    001 Ready with create, populate, and set exec bit on file.sh

--- create-logins.sh
+++ create-logins.sh
@@ -5,4 +5,11 @@ date=`date`;
 DOM=$1;
 DIR=~/s/ssh/logins;
 
-echo -e '#!/usr/bin/sh' "\n#$date\nssh envy@ns40.biz-land.com" $DIR/$DOM.sh
+echo -e '#!/usr/bin/sh' "\n#$date\nssh $DOM" > $DIR/$DOM.sh
+
+#
+chmod +x $DIR/$DOM.sh
+
+#
+cat $DIR/$DOM.sh;
+
