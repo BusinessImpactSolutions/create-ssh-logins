ff3eeda6e2e3f0a090e9d44d8fa011b725432009
root <root@ns70.biz-land.com>
2023-07-19 10:44:43 -0500

    002 Added if/else to create only if does not already exist and if already exists, login. Test OK

--- create-logins.sh
+++ create-logins.sh
@@ -5,11 +5,35 @@ date=`date`;
 DOM=$1;
 DIR=~/s/ssh/logins;
 
-echo -e '#!/usr/bin/sh' "\n#$date\nssh $DOM" > $DIR/$DOM.sh
+#
+PORT=$2;
+
+if [ $PORT = '' ]; then 
+	PORT='22';
+fi
+
+
+#
+#ssh-copy-id -P $PORT $DOM
+
+#
+if [ -e $DIR/$DOM.sh ]; then
+
+	echo "$DOM already exists";
+	echo "Logging in";
+	ssh -p $PORT $DOM;
+
+else
+	echo -e '#!/usr/bin/sh' "\n#$date\nssh -p $PORT $DOM" > $DIR/$DOM.sh
+fi
 
 #
 chmod +x $DIR/$DOM.sh
 
 #
 cat $DIR/$DOM.sh;
+
+#
+#ssh -p $PORT $DOM;
+
 
