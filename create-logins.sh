04cfcba45eb382d6cbabee14ef7e993f5a681079
root <root@ns70.biz-land.com>
2023-07-20 20:41:34 -0500

    011 added list-logins.sh added comments to crate-logins.sh

--- create-logins.sh
+++ create-logins.sh
@@ -1,31 +1,36 @@
 #!/usr/bin/sh
 
+#date
 date=`date`;
 
+#Directory to shore logins
 DIR=~/s/ssh/logins;
+
+# Path to your .ssh dir
 SSHDIR=~/.ssh;
 
-# Domain to login 
+# Domain to login: first item
 DOM=$1;
-# port for domain 
+
+# port for domain: second item
 PORT='';
 
 # custome port or 22
-if [ "$2" = '' ]; then 
+if [ "$2" = '' ]; then
 	PORT='22';
-else 
+else
 	PORT="$2";
 fi
 
 # run ssh-keygen if not already
-if [ -f $SSHDIR/id_ed25519.pub ]; then 
+if [ -f $SSHDIR/id_ed25519.pub ]; then
 	ssh-copy-id -p $PORT $DOM
-else 
+else
 	#
 	ssh-keygen -t ed25519;
 fi
 
-# create login script if does not exist 
+# create login script if does not exist
 if [ -e $DIR/$DOM.sh ]; then
 
 	echo "$DOM already exists";
@@ -33,13 +38,13 @@ if [ -e $DIR/$DOM.sh ]; then
 	$DIR/$DOM.sh;
 
 else
-	# create 
+	# create
 
 	# if port is empty, then it is 22 by default
-	if [ "$2" = '' ]; then 
+	if [ "$2" = '' ]; then
 		# create .sh file without port which will take 22 as default
 		echo -e '#!/usr/bin/sh' "\n#$date\nssh $DOM" > $DIR/$DOM.sh
-	else 
+	else
 		# else, create .sh file with custom port
 		echo -e '#!/usr/bin/sh' "\n#$date\nssh -p $PORT $DOM" > $DIR/$DOM.sh
 	fi
@@ -48,8 +53,7 @@ else
 	chmod +x $DIR/$DOM.sh
 	# show file after creation
 	cat $DIR/$DOM.sh;
-	# login after creating .sh file for domain 
+	# login after creating .sh file for domain
 	ssh -p $PORT $DOM;
 
 fi
-
---
+++ list-logins.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/sh
+
+ls ~/s/ssh/logins
